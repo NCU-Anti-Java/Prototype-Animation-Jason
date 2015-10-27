29a730a7e77bd7133829f94871ae3ec6760bce76
TEMU1086 <TEMU1086@gmail.com>
2015-10-27 17:59:31 +0800

    Add arrow key handling logics

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -14,7 +14,20 @@ public class MainWindow {
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
-                labelMain.setText(Integer.toString(e.getKeyCode()));
+                switch(e.getKeyCode()){
+                    case 37:
+                        labelMain.setText("Left");
+                        break;
+                    case 38:
+                        labelMain.setText("Up");
+                        break;
+                    case 39:
+                        labelMain.setText("Right");
+                        break;
+                    case 40:
+                        labelMain.setText("Down");
+                        break;
+                }
             }
         });
     }
